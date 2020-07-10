52bed32a9f55f8a06a47074c1d08d72bf5196a12
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-10 16:09:24 -0400

    6. Create queue using singly linked list-finished

--- README.md
+++ README.md
@@ -24,5 +24,13 @@
 
 5. Sort Stack
 
-#### In Progress
-6. 
+#### Finished
+6. Create a queue using a singly linked list
+    - Walk through the Queue class in the curriculum and understand it well. Then write a Queue class
+    - with its core functions (enqueue(), dequeue()) from scratch.
+    - Create a queue called starTrekQ and add Kirk, Spock, Uhura, Sulu, and Checkov to the queue.
+    - Implement a peek() function outside of the Queue class that lets you take a peek at what the
+    - 1st item in the queue is.
+    - Implement a isEmpty() function outside the Queue class that allows you to check if the queue is empty or not
+    - Implement a display() function outside of the Queue class that lets you display what's in the queue.
+    - Remove Spock from the queue and display the resulting queue.
--- queue.js
+++ queue.js
@@ -1,21 +1,90 @@
-function newQueue(){
-    console.log('hello')
-    const starTrek = new Stack();
 
-    // starTrek.push('Kirk')
-    // starTrek.push('Spock')
-    // starTrek.push('McCoy')
-    // starTrek.push('Scotty')
 
-    // let peekTop = peek(starTrek);
-    // let checkEmpty = isEmpty(starTrek);
-
-    // starTrek.pop();
-    // starTrek.pop();
-    // let displayStack = display(starTrek);
-    //let palindrome = is_palindrome(starTrek);
-
-    return ;
+class _Node_ {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-console.log(newQueue())
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    }
+
+    enqueue(data) {
+        const node = new _Node_(data);
+
+        if (this.first === null) {
+            this.first = node;
+        }
+
+        if (this.last) {
+            this.last.next = node;
+        }
+
+        this.last = node;
+    }
+
+    dequeue() {
+        if (this.first === null) {
+            return;
+        }
+        let node = this.first;
+        this.first = this.first.next;
+
+        if (node === this.last) {
+            this.last = null;
+        }
+        return node.value;
+    }
+}
+
+
+function _peek(stack){
+    return stack.first;
+}
+
+function _isEmpty(stack){
+    if(stack.first === null){
+        return 'empty'
+    }
+    else return 'not empty'
+}
+
+function _display(stack){
+
+    let stackArr = [];
+    let node = stack.first
+
+    while(node !== null){
+        if(node !== null){
+            stackArr.push(node.value)
+        }
+        node = node.next
+    }
+    return stackArr;
+}
+
+function newQueue() {
+    const starTrekQ = new Queue();
+
+    starTrekQ.enqueue('Kirk')
+    starTrekQ.enqueue('Spock')
+    starTrekQ.enqueue('Uhura')
+    starTrekQ.enqueue('Sulu')
+    starTrekQ.enqueue('Checkov')
+
+    let _peekTop = _peek(starTrekQ);
+    let _checkEmpty = _isEmpty(starTrekQ);
+
+    starTrekQ.dequeue();
+    starTrekQ.dequeue();
+    let _displayStack = _display(starTrekQ);
+    //let palindrome = is_palindrome(starTrekQ);
+
+    return _displayStack;
+}
+
+console.log(newQueue());
